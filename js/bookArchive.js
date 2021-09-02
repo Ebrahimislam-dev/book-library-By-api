63e1f929999277ee6c278ce44e90787cd741e199
ebrahimiiuc <ebrahimislam34@gmail.com>
2021-09-02 17:37:23 +0600

    upgarde design

--- js/bookArchive.js
+++ js/bookArchive.js
@@ -1,12 +1,14 @@
 const searchbook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    // console.log(searchText);
     searchField.value = "";
-    if (searchText == '') {
+    if (searchText === '') {
         const errormsg = document.getElementById('error-msg');
         errormsg.style.display = "block"
         document.getElementById('search-result').textContent = "";
+        document.getElementById('total-search').textContent = "";
+        document.getElementById('search-msg').textContent = "";
+
     }
 
     else {
@@ -22,38 +24,32 @@ const searchbook = () => {
 }
 const displaySearchResult = books => {
 
-
     const searchresultDiv = document.getElementById('search-result');
     searchresultDiv.textContent = "";
     const booksNumber = books.length;
-    document.getElementById('total-search').innerHTML = `<h4 class=" fw-bold text-center">About ${booksNumber} results.....</h4>`;
-
-
+    document.getElementById('total-search').innerHTML = `<h4 class=" fw-bold text-center">About ${booksNumber} Results Found.....</h4>`;
 
     if (books.length === 0) {
         alert("no books found")
-        // document.getElementById('error-msg2').style.display = 'block';
+
     };
 
-
-
-
-    books.slice(0, 15).forEach(book => {
+    books.slice(0, 21).forEach(book => {
         console.log(book);
         const div = document.createElement('div');
         div.classList.add("col");
         div.innerHTML = `
-        <div class="card h-100 mb-3" style="max-width: 450px;">
-        <div class="row g-0">
+        <div class="card h-100 mb-3">
+        <div class="row g-0 p-3 h-100 border-4 rounded shadow">
           <div class="col-md-4 ">
           <img src=" https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i : -1}-M.jpg" class="img-fluid rounded-start" alt="No image found">
           </div>
           <div class="col-md-8">
                 <div class="card-body">
-                    <h4 class=" fw-bold">${book.title}</h4>
-                    <h6>Author: ${book.author_name}</h6>
-                    <h6>Publisher: ${book.publisher}</h6>
-                    <p >Publish Year: ${book.first_publish_year}</p>
+                    <h4 class=" fw-bold">${book.title? book.title:"Not found"}</h4>
+                    <h6>Author: ${book.author_name ? book.author_name:"Not found"}</h6>
+                    <h6>Publisher: ${book.publisher ? book.publisher:"Not found"}</h6>
+                    <p >Publish Year: ${book.first_publish_year ? book.first_publish_year:"Not found"}</p>
                 </div>
           </div>
         </div>
